e69df0d995b385d83da1b745925559ea15ed8020
Betalov <86950046+Betalov@users.noreply.github.com>
2025-06-22 13:33:04 +0200

    Update init-TerrariaServer-arm64.sh

--- .scripts/init-TerrariaServer-arm64.sh
+++ .scripts/init-TerrariaServer-arm64.sh
@@ -2,4 +2,4 @@
 
 ./create-server-config.sh;
 
-mono --server --gc=sgen -O=all ./TerrariaServer.exe -config server-config.conf
+screen -dmS tml mono --server --gc=sgen -O=all ./TerrariaServer.exe -config server-config.conf
